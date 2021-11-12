249ae54e80f5841c1a1d75aa76a3c5bef356c5a0
JACQUENET Christophe <jjacquenet@myges.fr>
2021-11-12 14:50:04 +0100

    [Initial Commit]

---
+++ src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  constructor() {}
+
+  @Get('donne-moi-toutes-les-informations-d’un-utilisateur')
+  getFakeUserList() {
+    return [
+      {id: "ahgdvb45dfte54aYHs", firstname: "Myśliwiec", lastname: "Kamil", email: "example@sample.com", password: "NotSoSecret!99", dateOfBirth: "1001887200000"},
+      {id: "fpznfFZEdDI87HG", firstname: "SquarePants", lastname: "Sponge-Bob", email: "example@sample.com", password: "NotSoSecret!99", dateOfBirth: "1001788200000"},
+    ];
+  }
+}
